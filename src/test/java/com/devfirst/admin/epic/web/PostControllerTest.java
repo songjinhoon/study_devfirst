c0dfe767d61b9fa999fee1a15a8afa1909eb9c15
songjinhoon <jinhoon0319@gmail.com>
2021-03-14 18:07:38 +0900

    웹팩 셋팅 전 정리

--- src/test/java/com/devfirst/admin/epic/web/PostControllerTest.java
+++ src/test/java/com/devfirst/admin/epic/web/PostControllerTest.java
@@ -3,23 +3,23 @@ package com.devfirst.admin.epic.web;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = PostController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostControllerTest {
 
     @Autowired
-    private MockMvc mvc;
+    private TestRestTemplate restTemplate;
 
     @Test
     public void index_페이지() throws Exception {
-        mvc.perform(get("/post/list")) // contextpath 제외해야함.
-                .andExpect(status().isOk());
+        String body = this.restTemplate.getForObject("/post/list", String.class);
+        
+        assertThat(body).contains("하위하위");
     }
 }
